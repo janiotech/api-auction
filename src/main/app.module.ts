77210e108fbceec4cdc46fbcb9662e992caaef04
janio <sk8.merp@gmail.com>
2024-08-02 12:04:04 -0300

    add environment variables

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 //modules
 import { UsersModule } from '../users/users.module';
+//models
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'auction',
+      autoLoadModels: true,
+      synchronize: true,
+      models: [User],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
